ff4b36c15c161e2579046d566d7f275047e32e56
Anthony DeGarimore <anthony@pasoroblesconcierge.com>
2026-02-26 21:34:21 -0800

    Add configuration management for kiosk

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import asyncio
 
+from config import AcornCapConfig
 from kiosk import handle_kiosk
 from brightness import handle_screen_brightness
 
 
 async def main():
+    config = AcornCapConfig()
     tasks = [
-        asyncio.create_task(handle_kiosk()),
+        asyncio.create_task(handle_kiosk(config.kiosk)),
         asyncio.create_task(handle_screen_brightness()),
     ]
-
     await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
 
 
